28f1ed07f7e76ad95479fb96747361d8517f895c
Deepan455 <pradippandey455@gmail.com>
2024-09-18 09:08:36 +0545

    add url

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'localhost:5006', 'cloudflare.com', 'stanford.edu','http://202.51.83.166','https://202.51.83.166','kedc.edu.np'],
+    domains: ['localhost', 'localhost:5006', 'cloudflare.com', 'stanford.edu','http://202.51.83.166','https://202.51.83.166','kedc.edu.np','202.51.83.166'],
   },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
